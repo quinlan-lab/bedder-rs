688d238a96a18de13f5e7949e3010c12c165aa84
Brent Pedersen <bpederse@gmail.com>
2023-04-20 15:05:58 -0700

    add stub of trait methods for extracting data

--- src/position.rs
+++ src/position.rs
@@ -1,5 +1,23 @@
+pub enum Value {
+    Int(i64),
+    Float(f64),
+    String(String),
+    Ints(Vec<i64>),
+    Floats(Vec<f64>),
+    Strings(Vec<String>),
+}
+
 pub trait Positioned<'a> {
     fn position(&self) -> Position<'a>;
+
+    // extract a value from the Positioned object with a string key
+    //fn value(&self, String) -> Value<'a>
+
+    // extract a value from the Positioned object with an integer key. for a column.
+    //fn ivalue(&self, usize) -> Value<'a>
+
+    // get back the original line?
+    //fn line(&self) -> &'a str;
 }
 
 #[derive(Debug)]
