9bf4911a8d14dede76c80557d1dbe07017c4d1aa
Brent Pedersen <bpederse@gmail.com>
2023-05-03 14:19:24 -0700

    implement result type, add more tests and checks

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,2 @@
-pub mod bed_ex;
 pub mod intersection;
 pub mod position;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
