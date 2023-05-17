78268cb258ccadde5faf0f69c916c03dbdca7604
Brent Pedersen <bpederse@gmail.com>
2023-05-17 16:26:50 -0700

    use imports in bed example

--- examples/intersect_bed_count.rs
+++ examples/intersect_bed_count.rs
@@ -1,9 +1,11 @@
 use std::collections::HashMap;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, BufReader};
 use std::path::PathBuf;
 
 use clap::Parser;
 extern crate resort;
+use crate::resort::bedder_bed::BedderBed;
+use crate::resort::intersection::IntersectionIterator;
 use crate::resort::position::Positioned;
 use crate::resort::string::String;
 
@@ -18,13 +20,13 @@ struct Args {
 fn main() -> io::Result<()> {
     let args = Args::parse();
 
-    let a = std::io::BufReader::new(std::fs::File::open(args.a)?);
-    let b = std::io::BufReader::new(std::fs::File::open(args.b)?);
+    let a = BufReader::new(std::fs::File::open(args.a)?);
+    let b = BufReader::new(std::fs::File::open(args.b)?);
 
-    let ai = crate::resort::bedder_bed::BedderBed::new(a);
-    let bi = crate::resort::bedder_bed::BedderBed::new(b);
+    let ai = BedderBed::new(a);
+    let bi = BedderBed::new(b);
 
-    let fh = std::io::BufReader::new(std::fs::File::open(args.fai)?);
+    let fh = BufReader::new(std::fs::File::open(args.fai)?);
     let mut h = HashMap::new();
     fh.lines().enumerate().for_each(|(i, line)| {
         let line = line.expect("error reading line from fai");
@@ -32,7 +34,7 @@ fn main() -> io::Result<()> {
         h.insert(String::from(chrom), i);
     });
 
-    let it = crate::resort::intersection::IntersectionIterator::new(ai, vec![bi], &h)?;
+    let it = IntersectionIterator::new(ai, vec![bi], &h)?;
 
     for intersection in it {
         let intersection = intersection?;
