4675d1dfe19fc36a4bebbeda9cee034acb205e55
Brent Pedersen <bpederse@gmail.com>
2023-06-02 09:28:13 -0700

    crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,19 @@
+//! Bedder is a library for intersecting genomic data.
+
+/// Intersection iterators and data structures.
 pub mod intersection;
+
+/// Position traits.
 pub mod position;
+
+/// a std::String::String unless other string features are enabled.
 pub mod string;
 
 #[cfg(feature = "bed")]
+/// Bed parser implementing the PositionedIterator trait.
 pub mod bedder_bed;
 
+
 #[cfg(feature = "vcf")]
+/// Vcf parser implementing the PositionedIterator trait.
 pub mod bedder_vcf;
