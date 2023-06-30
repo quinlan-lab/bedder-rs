4f106838fb3048430c35796b641cfbc1afd48054
Brent Pedersen <bpederse@gmail.com>
2023-06-30 11:31:22 -0700

    more about the current design dilemma

--- examples/intersect_bed_count.rs
+++ examples/intersect_bed_count.rs
@@ -1,10 +1,11 @@
 use std::collections::HashMap;
+use std::fs;
 use std::io::{self, BufRead, BufReader};
 use std::path::PathBuf;
 
+use bedder::sniff;
 use clap::Parser;
 extern crate bedder;
-use crate::bedder::bedder_bed::BedderBed;
 use crate::bedder::intersection::IntersectionIterator;
 use crate::bedder::position::Positioned;
 use crate::bedder::string::String;
@@ -20,13 +21,13 @@ struct Args {
 fn main() -> io::Result<()> {
     let args = Args::parse();
 
-    let a = BufReader::new(std::fs::File::open(args.a)?);
-    let b = BufReader::new(std::fs::File::open(args.b)?);
+    // sniff determines the file type (bam/cram/bcf/vcf/bed/gff/gtf)
+    // and returns a PositionIterator
+    let ai = sniff::open_file(&args.a)?;
+    let bi = sniff::open_file(&args.b)?;
 
-    let ai = BedderBed::new(a);
-    let bi = BedderBed::new(b);
-
-    let fh = BufReader::new(std::fs::File::open(args.fai)?);
+    // bedder always requires a hashmap that indicates the chromosome order
+    let fh = BufReader::new(fs::File::open(args.fai)?);
     let mut h = HashMap::new();
     fh.lines().enumerate().for_each(|(i, line)| {
         let line = line.expect("error reading line from fai");
@@ -34,10 +35,28 @@ fn main() -> io::Result<()> {
         h.insert(String::from(chrom), i);
     });
 
-    let it = IntersectionIterator::new(Box::new(ai), vec![Box::new(bi)], &h)?;
+    // we can have any number of b (other_iterators).
+    let it = IntersectionIterator::new(ai, vec![bi], &h)?;
 
     for intersection in it {
         let intersection = intersection?;
+        // here we have the Positioned intersections (below we use the first() one)
+        // NOTE that as implemented, we'd have to have some other way to know
+        // that a given file is a VCF and has an AD field.
+        // see issue #15 for more discussion into using an enum.
+        // alternatively, we *can* know the file type from the sniff operation when opening.
+        // but this places more burden on the user of the API to track file-types.
+        intersection.overlapping.first().map(|p| {
+            let value = p
+                .interval
+                .value(bedder::position::Field::String(String::from("AD")))
+                .expect("hard coded AD field expecting VCF");
+            // extract the integer value
+            match value {
+                bedder::position::Value::Ints(i) => println!("AD: {:?}", i),
+                _ => panic!("expected integer"),
+            }
+        });
         println!(
             "{}\t{}\t{}\t{}",
             intersection.base_interval.chrom(),
