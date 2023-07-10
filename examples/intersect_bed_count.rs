79bb6d6cf2156cba5fa83d61e1d94d68c5153239
Brent Pedersen <bpederse@gmail.com>
2023-07-10 12:47:58 -0700

    simplify

--- examples/intersect_bed_count.rs
+++ examples/intersect_bed_count.rs
@@ -36,23 +36,6 @@ fn main() -> io::Result<()> {
 
     for intersection in it {
         let intersection = intersection?;
-        // here we have the Positioned intersections (below we use the first() one)
-        // NOTE that as implemented, we'd have to have some other way to know
-        // that a given file is a VCF and has an AD field.
-        // see issue #15 for more discussion into using an enum.
-        // alternatively, we *can* know the file type from the sniff operation when opening.
-        // but this places more burden on the user of the API to track file-types.
-        intersection.overlapping.first().map(|p| {
-            let value = p
-                .interval
-                .value(bedder::position::Field::String(String::from("AD")))
-                .expect("hard coded AD field expecting VCF");
-            // extract the integer value
-            match value {
-                bedder::position::Value::Ints(i) => println!("AD: {:?}", i),
-                _ => panic!("expected integer"),
-            }
-        });
         println!(
             "{}\t{}\t{}\t{}",
             intersection.base_interval.chrom(),
