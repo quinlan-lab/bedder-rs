a558aa577c16e0478f5d98bb7e70575bd41ec232
Brent Pedersen <bpederse@gmail.com>
2025-05-06 14:20:17 -0700

    add example.py file

---
+++ tests/examples/example.py
@@ -0,0 +1,15 @@
+#def bedder(fragment):
+#    if fragment.a.start % 2 != 0: return "odd"
+#    return "even"
+
+def bedder_odd(fragment) -> str:
+    """return odd if the start of the query interval is odd. otherwise even"""
+    if fragment.a.start % 2 != 0: return "odd"
+    return "even"
+
+def bedder_n_overlapping(fragment) -> int:
+    return len(fragment.b)
+
+
+def bedder_total_b_overlap(fragment) -> int:
+    return sum(b.stop - b.start for b in fragment.b)
